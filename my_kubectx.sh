df5a8d9acb6df8c1d458fb25c865aa440fe531fb
nooose <hd15807@gmail.com>
2022-06-22 13:44:21 +0900

    style: indent 4

--- my_kubectx.sh
+++ my_kubectx.sh
@@ -8,10 +8,10 @@ NUMBER_OF_KUBE_CONTEXTS=$((LINE_NUMBER_OF_KUBE_CONTEXTS - 1))
 
 
 kubectl config get-contexts \
-                                | awk '{if (NR > 1) print $2}' \
-                                | awk -v context="$CONTEXT_HOLDER" \
-                                                        '{if ($1 == context) {
-                                                                print "\033[34m[" NR "] " $1 "\033[0m"
+    | awk '{if (NR > 1) print $2}' \
+    | awk -v context="$CONTEXT_HOLDER" \
+    '{if ($1 == context) {
+        print "\033[34m[" NR "] " $1 "\033[0m"
                                                         } else {
                                                                 print "[" NR "] " $1
                                                         }}'
@@ -24,28 +24,28 @@ fi
 
 re='^[0-9]+$'
 if ! [[ $INPUT_CTX_NUMBER =~ $re ]]; then
-         echo "error: Not a number" >&2
-         exit 1
+     echo "error: Not a number" >&2
+     exit 1
 fi
 
 if [ $INPUT_CTX_NUMBER -lt 0 ] || [ $INPUT_CTX_NUMBER -gt $NUMBER_OF_KUBE_CONTEXTS ]; then
-	echo "error: Input valid number" >&2
-	exit 1
+    echo "error: Input valid number" >&2
+    exit 1
 fi
 
 
 if [ $INPUT_CTX_NUMBER -eq 0 ]; then
-        exit 0
+    exit 0
 fi
 
 
 for kubectx in `kubectl config get-contexts | awk '{if (NR > 1) print NR - 1 "," $2}'`
 do
-        context_number=${kubectx%,*}
-        context_name=${kubectx#*,}
+    context_number=${kubectx%,*}
+    context_name=${kubectx#*,}
 
-        if [ $context_number -eq $INPUT_CTX_NUMBER ] && [ $context_name != $CONTEXT_HOLDER ]; then
-                kubectl config use-context $context_name
-                exit 0
-        fi
+    if [ $context_number -eq $INPUT_CTX_NUMBER ] && [ $context_name != $CONTEXT_HOLDER ]; then
+        kubectl config use-context $context_name
+        exit 0
+    fi
 done
